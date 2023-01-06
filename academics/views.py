d7767ee40dd6ead2cf72bbe852e7b51b7eab8b09
MateusSantanna <mateussantanna2001@outlook.com>
2023-01-06 13:23:25 -0300

    fix: academic apps fixed

--- academics/views.py
+++ academics/views.py
@@ -14,6 +14,7 @@ class AcademicView(CreateAPIView):
     serializer_class = AcademicSerializer  
     queryset = Academic.objects.all()
 
+
 class AcademicDetailView(RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
