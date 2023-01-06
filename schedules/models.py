121454b48c7d8cf2538e2a86a5f272d90d78627f
Guilherme Bernardo <guilhermebernardo@id.uff.br>
2023-01-05 21:59:22 -0300

    FEAT: initial setup in serializers and views

--- schedules/models.py
+++ schedules/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 from uuid import uuid4
 
-class Scheduler(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, unique=True)
+options = (
+    ('WORK', 'Trabalho'),
+    ('SLEEP', 'Dormindo'),
+    ('STUDY', 'Estudo'),
+    ('HOBBY', 'Hobby'),
+    ('HEALTH', 'Saúde'),
+    ('COMMITMENT', 'Compromisso')
+)
+
+class Schedule(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid4, 
+        unique=True
+    )
     title = models.CharField(max_length=128)
-    type = models.ChoiceField(max_length=16, default="commitment")
+    type = models.ChoiceField(
+        max_length=16, 
+        choices=options,
+        default="COMMITMENT"
+    )
     begans_at = models.DateTimeField()
     ends_at = models.DateTimeField(defaut=begans_at)
     routine_weekdays = models.CharField(max_length=23)
--- schedules/views.py
+++ schedules/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from .models import Schedule
+from .serializers import ScheduleSerializer
+from users.permissions import IsAccountOwnerOrSuperuser 
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+
+class ScheduleListCreateView(ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwnerOrSuperuser]
+    
+    queryset = Schedule.objects.all()
+    serializer_class = ScheduleSerializer
+
+class ScheduleRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwnerOrSuperuser]
+    
+    queryset = Schedule.objects.all()
+    serializer_class = ScheduleSerializer
