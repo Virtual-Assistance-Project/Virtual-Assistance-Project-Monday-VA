f79d7ea99bd46a5ad7669b25c0d2495c5f22c9f3
axdborges <axdborges@outlook.com>
2023-01-09 08:54:50 -0300

    Feat(/daily_quotas): creating daily_quotas view

--- daily_quotas/views.py
+++ daily_quotas/views.py
@@ -11,6 +11,28 @@ class DailyQuotaView(CreateAPIView):
     permission_classes = [IsAccountOwnerOrSuperuser]
     serializer_class = DailyQuotaSerializer
 
+    def perform_create(self, serializer: DailyQuotaSerializer):
+
+        """
+            vai receber uma lista de dicionários com nome da quota e numero totais de horas
+            para poder transformas essas horas em decimais que significam %
+
+            Horas totais são 24 então o cálculo para tansformar é pegar o número de horas inteiros
+            e dividir por 24
+
+            Mudar o serializer ou não? 
+        """
+        quota = { 
+            "work_percentage": self.kwargs["work"] / 24,
+            "sleep_percentage": self.kwargs["sleep"] / 24, 
+            "sludy_percentage": self.kwargs["sludy"] / 24, 
+            "hobby_percentage": self.kwargs["hobby"] / 24, 
+            "health_percentage": self.kwargs["health"] / 24, 
+        }
+        
+        
+        serializer.save(quota)
+
 
 class DailyQuotaDetailView(RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
