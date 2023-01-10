3a7f1e70724c231ae51c4c75dbff7e98b3f8ad3a
Igor Gomes <igorgomes255@gmail.com>
2023-01-10 10:33:07 -0300

    FIX: fixing url in finance

--- finance_infos/urls.py
+++ finance_infos/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("finance/", views.FinanceView.as_view()),
-    path("finance/<uuid:finance_id>", views.FinanceDetails.as_view()),
+    path("infos/finance/", views.FinanceView.as_view()),
+    path("infos/finance/profile/", views.FinanceDetails.as_view()),
 ]
